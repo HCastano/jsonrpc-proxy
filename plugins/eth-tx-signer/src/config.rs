87e69a8b755e4e6961c9df7003bc6458893330ca
Hernando Castano <castano.ha@gmail.com>
2019-02-21 16:20:49 +0100

    Add config options for Eth signer

--- plugins/eth-tx-signer/src/config.rs
+++ plugins/eth-tx-signer/src/config.rs
@@ -1,8 +1,31 @@
-/// Configuration options of an upstream
-#[derive(Clone, Debug)]
-pub enum Param {}
+//! CLI configuration for Ethereum signer.
 
-/// Returns all configuration parameters for WS upstream.
+#[derive(Clone, Debug)]
+pub enum Param {
+    /// Path to JSON keystore file
+    Config(String),
+}
+
+/// Returns all configuration parameters for Eth signer.
 pub fn params() -> Vec<cli_params::Param<Param>> {
-    unimplemented!();
+    vec![
+        cli_params::Param::new(
+            "Ethereum Transaction Signer",
+            "eth-tx-signer-config",
+            "A path to a JSON file containing keys.",
+            "-",
+            |path: String| {
+                if &path == "-" {
+                    // Will want to default to this directory
+                    // Or maybe the std. Parity Eth keystore?
+                    return Ok(Param::Config(".".into()))
+                }
+
+                // Just want to return the path to the file
+                // Will deal with parsing the file later (during the
+                // signing step)
+                Ok(Param::Config(path))
+            }
+        )
+    ]
 }
